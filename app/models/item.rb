2b917c6fae70da57de09ce120d3e8a9780edd681
Markus Leyendecker <markus.c.leyendecker@gmail.com>
2015-10-02 12:22:36 +0200

    adjusted image size

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ActiveRecord::Base
   validates :name, presence: true
   validates :description, presence: true
 
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/items/:id.png"
+  has_attached_file :image, styles: { medium: "500x500>", thumb: "100x100>" }, default_url: "/images/items/:id.png"
   validates :image, attachment_presence: true
   validates_attachment_content_type :image, content_type: ['image/jpeg', 'image/png', 'image/gif', 'application/pdf']
   validates_with AttachmentSizeValidator, attributes: :image, less_than: 3.megabytes
