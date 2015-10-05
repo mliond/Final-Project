e411839dd081e6f9cffdfcbf6d6365118e1170d7
Markus Leyendecker <markus.c.leyendecker@gmail.com>
2015-10-05 18:46:55 +0200

    played around with devise as JSON

---
+++ app/assets/javascripts/users/login.js
@@ -0,0 +1,20 @@
+$('a#login').on('click', function(event) {
+  event.preventDefault();
+  $('div#item-info').hide();
+  $('div#login').show();
+
+  $.ajax({ url: '/users/sign_in',
+    type: 'POST',
+    beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
+    data: {user: {email: "j@j.de", password: "1"}},
+    success: onSuccess,
+    error: function(response) {
+      debugger
+      console.log(response);
+    }
+  });
+})
+
+function onSuccess(data) {
+  debugger
+}
