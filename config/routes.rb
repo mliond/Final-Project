70ad2c8eab0676171501b7d7fc009cb2a6f625ed
Markus Leyendecker <markus.c.leyendecker@gmail.com>
2015-10-06 10:20:41 +0200

    removed devise AJAX from yesterday

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
-  # devise_for :users
-  devise_for :users, :controllers => {sessions: 'sessions', registrations: 'registrations'}
+  devise_for :users
 
   resources :items
 
