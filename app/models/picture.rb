13fee7969098f114d5ceb8efd5b44bd3daa50083
Markus Leyendecker <markus.c.leyendecker@gmail.com>
2015-10-07 14:32:12 +0200

    stable / AWS for pictures

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -8,12 +8,9 @@ class Picture < ActiveRecord::Base
     styles: { medium: "300x300>", thumb: "100x100>" },
     :storage => :s3,
     :s3_credentials => {
-      :bucket => "testbucketfinalproject", #ENV['S3_BUCKET_NAME'],
-      :access_key_id =>  "AKIAILJYHDRVKHDAW6PQ", #ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => "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" #ENV['AWS_SECRET_ACCESS_KEY']
-      # :bucket => ENV['S3_BUCKET_NAME'],
-      # :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      # :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   validates :image, attachment_presence: true
   validates_attachment_content_type :image, content_type: ['image/jpeg', 'image/png', 'image/gif', 'application/pdf']
