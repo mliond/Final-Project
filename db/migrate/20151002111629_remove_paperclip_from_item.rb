428b46338e7133204924c6fc99f29b1c817fc120
Markus Leyendecker <markus.c.leyendecker@gmail.com>
2015-10-08 16:12:59 +0200

    update migration

--- db/migrate/20151002111629_remove_paperclip_from_item.rb
+++
@@ -1,8 +0,0 @@
-class RemovePaperclipFromItem < ActiveRecord::Migration
-  def change
-    remove_column :items, :image_file_name, :string
-    remove_column :items, :image_content_type, :string
-    remove_column :items, :image_file_size, :integer
-    remove_column :items, :image_updated_at, :datetime
-  end
-end
