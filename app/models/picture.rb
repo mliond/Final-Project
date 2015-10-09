27ee83f76b48b10d08942e17783c6834001f9149
Markus Leyendecker <markus.c.leyendecker@gmail.com>
2015-10-09 11:35:29 +0200

    env variables

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -8,9 +8,9 @@ class Picture < ActiveRecord::Base
     :styles => {medium: "500x500>", thumb: "100x100>"},
     :storage => :s3,
     :s3_credentials => {
-      :bucket =>  "picstorage", #ENV['S3_BUCKET_NAME'],
-      :access_key_id => "AKIAJJY5XTF6IFZ4XLJQ", #ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => "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" #ENV['AWS_SECRET_ACCESS_KEY']
+      :bucket =>  "ENV['S3_BUCKET_NAME']",
+      :access_key_id => "ENV['AWS_ACCESS_KEY_ID']",
+      :secret_access_key => "ENV['AWS_SECRET_ACCESS_KEY']"
     }
   validates :image, attachment_presence: true
   validates_attachment_content_type :image, content_type: ['image/jpeg', 'image/png', 'image/gif', 'application/pdf']
